188dc5a7fc195932a166744a18fe154b209b9721
Franck MARCEL <Franck MARCEL>
2015-12-03 18:01:21 +0100

    Squelette du site

---
+++ application/views/welcome_message.php
@@ -0,0 +1,21 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Nuit de l'info</title>
+
+</head>
+<body>
+
+<div id="container">
+	<h1>Site pour la nuit de l'info</h1>
+
+	<div id="body">
+		<p>En attente de sujet...</p>
+	</div>
+</div>
+
+</body>
+</html>
