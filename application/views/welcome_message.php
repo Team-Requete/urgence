59f1105b71c5d1f954d44796da9f237548eeef12
shall0531 <shall0531@hotmail.com>
2015-12-04 04:09:48 +0100

    add menu and block

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -1,6 +1,6 @@
-<?php
+<!--<?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-?><!DOCTYPE html>
+?><!DOCTYPE html>-->
 <html lang="en">
 <head>
 	<meta charset="utf-8">
@@ -10,12 +10,44 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <body>
 
 <div id="container">
-	<h1>Site pour la nuit de l'info</h1>
+	<div id="navbar" class="navbar-collapse collapse">
+	<a class="navbar-brand" href="#">Urgence</a>
+          <form class="navbar-form navbar-right">
+            <input type="text" class="form-control" placeholder="Search...">
+          </form>
+          
+        </div>
 
 	<div id="body">
-		<p>En attente de sujet...</p>
+		<div class="col-sm-3 col-md-2 sidebar">
+		          <ul class="nav nav-sidebar">
+		            <li><a href="#">Inscription</a></li>
+		            <li><a href="#">Log In</a></li>
+		            <li><a href="#">Help Anymous</a></li>
+		            <li><a href="#">Organize helps</a></li>
+		          </ul>
+		 </div>
+		<div>
+			<pre>Ce que l'on fait permet</pre>
+		</div>
+		<div>
+			<pre>A propos de la Team Requete</pre>
+		</div>
 	</div>
 </div>
 
 </body>
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
 </html>
